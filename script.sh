093217ea767972592bc187ef9321c5d96d5dc5c1
mickaelottmann <mickaelottmann2@gmail.com>
2025-07-14 13:42:48 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -11,6 +11,12 @@ echo "✅ Chromium installé avec succès."
 
 # Optionnel : export variable cache Puppeteer (si besoin)
 export PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+if [[ ! -d $PUPPETEER_CACHE_DIR]]; then
+    cp -r /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+else
+    cp -r $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+
+fi
 echo "📂 Variable PUPPETEER_CACHE_DIR définie : $PUPPETEER_CACHE_DIR"
 
 echo "▶️ Démarrage de l'application Node.js..."
